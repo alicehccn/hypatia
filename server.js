13b2f7bc677cd49144cc3a4c7963466bd911c313
Alice Huang <alice@expertise.com>
2017-08-30 16:22:34 -0700

    Listen to a different port

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const app = require('./app');
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 3001;
 
 const server = app.listen(port, () => {
   console.log(`Express server listening on port ${port}`)
