d212b7f0501a6dc8b6b6b0547ecc4fecd42d462b
Alice Huang <alice@expertise.com>
2017-09-01 09:31:33 -0700

    Make all fields required true

--- User.js
+++ User.js
@@ -2,9 +2,9 @@
 
 const mongoose = require('mongoose');
 const UserSchema = new mongoose.Schema({
-  name: String,
-  email: String,
-  password: String
+  name: {type: String, required: true},
+  email: {type: String, required: true},
+  password: {type: String, required: true}
 });
 mongoose.model('User', UserSchema);
 
