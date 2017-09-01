127aa6260133808b462ddee9883fd8387c035c6f
Alice Huang <alice@expertise.com>
2017-09-01 13:58:53 -0700

    Use bluebird as promise plugin

--- db.js
+++ db.js
@@ -1,4 +1,5 @@
 'use strict';
 
 const mongoose = require('mongoose');
+mongoose.Promise = require('bluebird');
 mongoose.connect('mongodb://admin:sophiebella@ds163613.mlab.com:63613/hypatia', { useMongoClient: true });
